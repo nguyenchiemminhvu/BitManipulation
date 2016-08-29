2540beaace06be86f3c28c9bfa7aadbdc604272d
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2016-08-29 16:24:49 +0700

    Bit handler class

---
+++ BitHandler.cpp
@@ -0,0 +1,145 @@
+#include "BitHandler.h"
+
+
+Bit::BitHandler::BitHandler()
+{
+}
+
+
+Bit::BitHandler::~BitHandler()
+{
+}
+
+
+bool Bit::BitHandler::isEven(unsigned __int32 n)
+{
+	return !(n & 1);
+}
+
+
+bool Bit::BitHandler::isOdd(unsigned __int32 n)
+{
+	return (n & 1);
+}
+
+
+bool Bit::BitHandler::isPowerOf2(unsigned __int32 n)
+{
+	return n && !(n & (n - 1));
+}
+
+
+unsigned __int32 Bit::BitHandler::enableBit(unsigned __int32 n, int position)
+{
+	n |= 1 << position;
+
+	return n;
+}
+
+
+unsigned __int32 Bit::BitHandler::disableBit(unsigned __int32 n, int position)
+{
+	n &= !(1 << position);
+
+	return n;
+}
+
+
+unsigned __int32 Bit::BitHandler::flipBit(unsigned __int32 n, int position)
+{
+	n ^= 1 << position;
+
+	return n;
+}
+
+
+unsigned __int32 Bit::BitHandler::flipAllBits(unsigned __int32 n)
+{
+	n ^= 0xffffffff;
+
+	return n;
+}
+
+
+bool Bit::BitHandler::isBitEnabled(unsigned __int32 n, int position)
+{
+	//return (n & (1 << position)) >> position;
+	return (n >> position) & 1;
+}
+
+
+bool Bit::BitHandler::isBitDisabled(unsigned __int32 n, int position)
+{
+	return !((n >> position) & 1);
+}
+
+
+unsigned __int32 Bit::BitHandler::nextPowerOf2(unsigned __int32 n)
+{
+	if (n && !(n & (n - 1)))
+		return n;
+
+	unsigned __int32 result = 1;
+	while (result < n)
+	{
+		result <<= 1;
+	}
+
+	return result;
+}
+
+
+bool Bit::BitHandler::isOddParity(unsigned __int32 n)
+{
+	bool parity = false;
+
+	while (n)
+	{
+		n = n & (n - 1);
+		parity = !parity;
+	}
+
+	return parity;
+}
+
+
+bool Bit::BitHandler::isEvenParity(unsigned __int32 n)
+{
+	bool parity = true;
+
+	while (n)
+	{
+		n = n & (n - 1);
+		parity = !parity;
+	}
+
+	return parity;
+}
+
+
+unsigned __int32 Bit::BitHandler::countDifferentBits(unsigned __int32 n1, unsigned __int32 n2)
+{
+	unsigned __int32 count = 0;
+
+	while (n1 && n2)
+	{
+		count += (n1 & 1) ^ (n2 & 1);
+
+		n1 >>= 1;
+		n2 >>= 1;
+	}
+
+	while (n1)
+	{
+		count++;
+		n1 >>= 1;
+	}
+
+	while (n2)
+	{
+		count++;
+		n2 >>= 1;
+	}
+
+	return count;
+}
---
+++ BitHandler.h
@@ -0,0 +1,33 @@
+#ifndef __BIT_HANDLER_H__
+#define __BIT_HANDLER_H__
+
+#pragma once
+
+namespace Bit
+{
+
+	class BitHandler
+	{
+	public:
+		BitHandler();
+		virtual ~BitHandler();
+
+		static bool isEven(unsigned __int32 n);
+		static bool isOdd(unsigned __int32 n);
+		static bool isPowerOf2(unsigned __int32 n);
+
+		static unsigned __int32 enableBit(unsigned __int32 n, int position);
+		static unsigned __int32 disableBit(unsigned __int32 n, int position);
+		static unsigned __int32 flipBit(unsigned __int32 n, int position);
+		static unsigned __int32 flipAllBits(unsigned __int32 n);
+		static bool isBitEnabled(unsigned __int32 n, int position);
+		static bool isBitDisabled(unsigned __int32 n, int position);
+
+		static unsigned __int32 nextPowerOf2(unsigned __int32 n);
+		static bool isOddParity(unsigned __int32 n);
+		static bool isEvenParity(unsigned __int32 n);
+		static unsigned __int32 countDifferentBits(unsigned __int32 n1, unsigned __int32 n2);
+	};
+
+}
+#endif //__BIT_HANDLER_H__
